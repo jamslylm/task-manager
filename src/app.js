c1c182bebad09cc83f0841f75d0680dc51bef3ae
Jamsly-love MINE <mjamsly98@gmail.com>
2021-11-20 03:55:33 -0500

    Refactored to be clean and small

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+require("./database/mongoose.js")
+const userRouter = require("./router/user")
+const taskRouter = require("./router/task")
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log("⚡[Running] Server is running up on port : " + port)
+})
