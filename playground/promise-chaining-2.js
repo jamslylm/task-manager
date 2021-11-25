a9c2520508da834692105df7edffec29c02771c1
Jamsly-love MINE <mjamsly98@gmail.com>
2021-11-25 14:34:58 -0500

    Working with promise based function

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,17 @@
+require("../src/database/mongoose");
+const Task = require("../src/models/Task");
+
+const _id = "61903125ae59925b21f41ebb";
+
+const deleteTaskAndCount = async (id) => {
+    const deleted = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return { deleted, count };
+};
+
+deleteTaskAndCount(_id)
+    .then((obj) => {
+        console.log(obj);
+    }).catch((error) => {
+    console.log(error);
+});
---
+++ playground/promise-chaining.js
@@ -0,0 +1,27 @@
+require("../src/database/mongoose");
+const User = require("../src/models/User");
+
+const _id = "618fae884dc92f2a30bbb1a7";
+
+/*User.findByIdAndUpdate(_id, { age: 0 })
+    .then((user) => {
+        console.log(user);
+        return User.countDocuments({ age: 0 });
+    }).then((resutl) => {
+    console.log(resutl);
+}).catch((error) => {
+    console.log(error);
+});*/
+
+const updateAgeCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return { user, count };
+};
+
+updateAgeCount(_id, 5)
+    .then((obj) => {
+        console.log(obj);
+    }).catch((e) => {
+    console.log(e);
+});
